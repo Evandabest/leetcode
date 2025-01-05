b9ca782de18da2b972f1372e41651dc00e8047bd
Evandabest <evanhaque1@gmail.com>
2025-01-04 19:09:12 -0500

    group anagrams

---
+++ groupAnagrams.py
@@ -0,0 +1,23 @@
+
+
+import typing
+
+# Leetcode #49: Group Anagrams
+
+
+class Solution:
+    def groupAnagrams(self, strs: typing.List[str]) -> List[List[str]]:
+
+        map = {}
+        
+        for i in strs:
+            chars = list(i)
+            sortedChars = sorted(chars)
+            joined = ("").join(sortedChars)
+            
+            if joined in map.keys():
+                map[joined].append(i)
+            else:
+                map[joined] = [i]
+        
+        return list(map.values())
